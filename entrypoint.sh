a3979c9a9d6eff3ac9ba877ee032726f80d8448f
Jacob O'Mara <jacobomara901@gmail.com>
2024-11-07 10:06:25 +0000

    feat: add API creds to .env template to allow login
    This adds the api creds to the env variables allowing us to log in without having to mount the expo.env file in to the container.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Write environment variables to .env file
+echo "API_KEY_1=$API_KEY_1" > .env
+echo "API_KEY_2=$API_KEY_2" > .env
+echo "API_KEY_3=$API_KEY_3" > .env
+echo "API_KEY_4=$API_KEY_4" > .env
+echo "API_KEY_5=$API_KEY_5" > .env
+
 # Make start.sh executable
 chmod +x ./start.sh
 
